33dd314ff360e0c61432b75f864d50f6f4d91013
pasumaibharatam <pasumaibharatam@gmail.com>
2026-01-14 22:03:21 +0530

    col

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -31,7 +31,7 @@ const AdminDashboard = () => {
 
   // Fetch candidates from backend
   useEffect(() => {
-    fetch("https://political-backend-wvrc.onrender.com/admin")
+    fetch("https://pol-bk.onrender.com/admin")
       .then(res => {
         if (!res.ok) throw new Error("Failed to fetch");
         return res.json();
